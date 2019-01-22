957bf3721b31989362dfa13b1a2dadf04b041a12
filinrey <1796826100@qq.com>
2019-01-22 21:27:53 +0800

    initial some cd codes

--- python/xcd.py
+++ python/xcd.py
@@ -8,15 +8,43 @@ import os.path
 import shutil
 from xdefine import XKey, xkey_to_str
 from xlogger import xlogger
-
-def action_xcd(cmd, key):
-    print ('\r')
-    print ('\r', end='')
-    print ('\t# cd, act as {}'.format(xkey_to_str(key)))
+from xprint import xprint_new_line, xprint_head
 
 
+def show_install_help():
+    xprint_new_line('\t# cd [ACTION]')
+    xprint_head('\tExample 1: # cd install')
+    xprint_head('\t           -> only support one ACTION as install currently')
+    xprint_head('\t           -> it will install a command named xcd which can be used directly in console')
+    xprint_head('\t           -> detail for xcd to be continued')
+
+
+def install_xcd():
+    xprint_new_line('\tinstalling xcd')
+    xprint_new_line('\tto be continued')
+
+
+def action_install(cmds, key):
+    num_cmd = len(cmds)
+    if cmds[num_cmd - 1] == '':
+        del cmds[num_cmd - 1]
+        num_cmd -= 1
+
+    if num_cmd == 0 and key == XKey.ENTER:
+        install_xcd()
+        return {'flag': True, 'new_input_cmd': ''}
+
+    show_install_help()
+
+
+# active is optional, [no active item] = [active is True]
 xcd_action = {
     'name': 'cd',
-    'action': action_xcd,
-    'active': False
+    'active': True,
+    'sub_cmds': [
+        {
+            'name': 'install',
+            'action': action_install,
+        }
+    ]
 }

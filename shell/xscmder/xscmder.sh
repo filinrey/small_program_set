a5e6e3b0c57179229dfd1bb6e023d06093d805e6
fenghxu <fenghuai.xu@nokia-sbell.com>
2019-01-24 13:38:06 +0800

    check tab/enter/backspace/arrow key in shell xcmder

--- shell/xscmder/xscmder.sh
+++ shell/xscmder/xscmder.sh
@@ -16,29 +16,69 @@ function get_key()
     stty -raw
 }
 
+function handle_enter_key()
+{
+    echo "enter"
+}
+
+function handle_tab_key()
+{
+    echo "tab"
+}
+
+function handle_backspace_key()
+{
+    echo "backspace"
+}
+
+function handle_arrow_key()
+{
+    key=$1
+    flag=$2
+
+    if [[ '' == $key && 0 == $flag ]]; then
+        return 1
+    fi
+    if [[ '[' == $key && 1 == $flag ]]; then
+        return 2
+    fi
+    if [[ 2 == $flag ]]; then
+        if [[ 'A' == $key ]]; then
+            echo "UP"
+        fi
+        if [[ 'B' == $key ]]; then
+            echo "DOWN"
+        fi
+        if [[ 'D' == $key ]]; then
+            echo "LEFT"
+        fi
+        if [[ 'C' == $key ]]; then
+            echo "RIGHT"
+        fi
+    fi
+    return 0
+}
+
 trap "xexit;" INT QUIT
 
+esc_flag=0
 c=' '
 while [ 1 ]
 do
     c=`get_key`
-    case "$c" in
-        '' ) echo "enter" ;;
-        '	' ) echo "tab" ;;
-        'q' ) xexit ;;
-        '' ) xexit ;;
-        '' )
-            echo "possible arrow keys"
-            secondchar=`get_key`
-            echo "secondchar:$secondchar"
-            thirdchar=`get_key`
-            echo "thirdchar:$thirdchar"
-            case "$thirdchar" in
-                'A' ) echo UP ;;
-                'B' ) echo DOWN ;;
-                'D' ) echo LEFT ;;
-                'C' ) echo RIGHT ;;
-                * ) echo "third *" ;;
-            esac ;;
-    esac
+ 
+    if [[ 'q' == $c || '' == $c ]]; then
+        xexit
+    fi
+    handle_arrow_key $c $esc_flag
+    let esc_flag=$?
+    if [[ '' == $c ]]; then
+        handle_enter_key
+    fi
+    if [[ '	' == $c ]]; then
+        handle_tab_key
+    fi
+    if [[ '' == $c ]]; then
+        handle_backspace_key
+    fi
 done

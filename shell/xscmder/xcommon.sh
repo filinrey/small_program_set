78202aa6fe0aa82e5e4c5110186b76d04a1066b8
fenghxu <fenghuai.xu@nokia-sbell.com>
2019-01-30 20:11:19 +0800

    [shell] handle left / right key

--- shell/xscmder/xcommon.sh
+++ shell/xscmder/xcommon.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/bash
 
 x_cur_dir=$(cd $(dirname $0); pwd)
-echo "cur_dir = $x_cur_dir"
 
 x_cur_file=`echo "$(basename $0)" | awk -F '.' '{print $1}'`
-echo "cur_file = $x_cur_file"
+x_prefix_name="$x_cur_file# "
 
 x_data_dir="$x_cur_dir/data"
 if [[ ! -d $x_data_dir ]]; then
@@ -15,60 +14,60 @@ x_log_file="$x_data_dir/$x_cur_file.log"
 x_login_history="$x_data_dir/login_history"
 x_cmd_history="$x_data_dir/cmd_history"
 
-function xlogger()
+declare -A xlogger_log_list
+xlogger_expect=()
+xlogger_expect_index=0
+
+function xlogger_fill_log_list()
 {
-    declare -A log_list
-    expect=("DATE" "LEVEL" "FILE" "LINE" "INFO")
-    cur_date=`date +%Y-%m-%d\ %H:%M:%S,%N`
-    log_list+=([${expect[0]}]="$cur_date")
-    echo ${log_list[${expect[0]}]}
-    expect_index=1
-    BACK_IFS=$IFS
+    local BACK_IFS=$IFS
     IFS=""
     for i in $@; do
-        key=${expect[$expect_index]}
-        key_value=($i)
-        log_list+=(["$key"]="$key_value")
-        let expect_index=expect_index+1
+        local key=${xlogger_expect[$xlogger_expect_index]}
+        local key_value=($i)
+        #echo "$key : $key_value"
+        xlogger_log_list+=(["$key"]="$key_value")
+        let xlogger_expect_index=xlogger_expect_index+1
     done
     IFS=$BACK_IFS
-    for i in $(seq $expect_index $((${#expect[@]}-1))); do
-        log_list+=([${expect[$expect_index]}]="")
+    for i in $(seq $xlogger_expect_index $((${#xlogger_expect[@]}-1))); do
+        xlogger_log_list+=([${xlogger_expect[$xlogger_expect_index]}]="")
     done
+}
 
-    let expect_index=0
-    log_info="${log_list[${expect[$expect_index]}]} "
-    let expect_index=expect_index+1
-    log_info="$log_info[${log_list[${expect[$expect_index]}]}] "
-    let expect_index=expect_index+1
-    log_info="$log_info${log_list[${expect[$expect_index]}]}:"
-    let expect_index=expect_index+1
-    log_info="$log_info${log_list[${expect[$expect_index]}]} "
-    let expect_index=expect_index+1
-    log_info="$log_info${log_list[${expect[$expect_index]}]}"
+function xlogger()
+{
+    xlogger_expect=("DATE" "LEVEL" "FILE" "LINE" "INFO")
+    cur_date=`date +%Y-%m-%d\ %H:%M:%S,%N`
+    xlogger_log_list+=([${xlogger_expect[0]}]="$cur_date")
+    xlogger_expect_index=1
+    local BACK_IFS=$IFS
+    IFS=""
+    xlogger_fill_log_list $@
+    IFS=$BACK_IFS
+
+    let xlogger_expect_index=0
+    local log_info="${xlogger_log_list[${xlogger_expect[$xlogger_expect_index]}]} "
+    let xlogger_expect_index=xlogger_expect_index+1
+    log_info="$log_info[${xlogger_log_list[${xlogger_expect[$xlogger_expect_index]}]}] "
+    let xlogger_expect_index=xlogger_expect_index+1
+    log_info="$log_info${xlogger_log_list[${xlogger_expect[$xlogger_expect_index]}]}:"
+    let xlogger_expect_index=xlogger_expect_index+1
+    log_info="$log_info${xlogger_log_list[${xlogger_expect[$xlogger_expect_index]}]} "
+    let xlogger_expect_index=xlogger_expect_index+1
+    log_info="$log_info${xlogger_log_list[${xlogger_expect[$xlogger_expect_index]}]}"
     echo "$log_info" >> $x_log_file
 }
 
 function xlogger_debug()
 {
-    declare -A debug_log_list
-    expect=("FILE" "LINE" "INFO")
-    expect_index=0
-    BACK_IFS=$IFS
+    xlogger_expect=("FILE" "LINE" "INFO")
+    xlogger_expect_index=0
+    local BACK_IFS=$IFS
     IFS=""
-    for i in $@; do
-        key=${expect[$expect_index]}
-        key_value=($i)
-        echo "$key : $key_value"
-        debug_log_list+=(["$key"]="$key_value")
-        let expect_index=expect_index+1
-    done
+    xlogger_fill_log_list $@
     IFS=$BACK_IFS
-    echo "expect_index = $expect_index, len = $((${#expect}-1)), ${#expect[@]}"
-    for i in $(seq $expect_index $((${#expect[@]}-1))); do
-        debug_log_list+=([${expect[$expect_index]}]="")
-    done
-    xlogger "DEBUG" "${debug_log_list[${expect[0]}]}" "${debug_log_list[${expect[1]}]}" "${debug_log_list[${expect[2]}]}"
+    xlogger "DEBUG" "${xlogger_log_list[${xlogger_expect[0]}]}" "${xlogger_log_list[${xlogger_expect[1]}]}" "${xlogger_log_list[${xlogger_expect[2]}]}"
 }
 
 #xlogger_debug "test.sh" 11 "debug info"
--- shell/xscmder/xscmder.sh
+++ shell/xscmder/xscmder.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/bash
 
+source xcommon.sh
+
+file_name="xscmder.sh"
+
+is_backspace_key=0
+is_left_right_key=0
+is_arrow_key=0
+
+cur_pos=0
+input_cmd=""
+esc_time=0
+
 origin_stty_config=`stty -g`
 stty -echo
 
@@ -33,30 +45,64 @@ function handle_backspace_key()
 
 function handle_arrow_key()
 {
-    key=$1
-    flag=$2
+    local key=$1
+    local flag=$2
 
+    let is_arrow_key=1
     if [[ '' == $key && 0 == $flag ]]; then
+        esc_time=`date +%s`
+        let is_left_right_key=0
         return 1
-    fi
-    if [[ '[' == $key && 1 == $flag ]]; then
+    elif [[ '[' == $key && 1 == $flag ]]; then
+        let is_left_right_key=0
+        cur_time=`date +%s`
+        xlogger_debug "$file_name" $LINENO "esc_time=$esc_time, cur_time=$cur_time, diff=$((cur_time-esc_time))"
+        if [[ $((cur_time-esc_time)) -gt 1 ]]; then
+            let is_arrow_key=0
+            return 0
+        fi
         return 2
-    fi
-    if [[ 2 == $flag ]]; then
+    elif [[ 2 == $flag ]]; then
+        cur_time=`date +%s`
+        xlogger_debug "$file_name" $LINENO "esc_time=$esc_time, cur_time=$cur_time, diff=$((cur_time-esc_time))"
+        if [[ $((cur_time-esc_time)) -gt 1 ]]; then
+            let is_arrow_key=0
+            let is_left_right_key=0
+            return 0
+        fi
         if [[ 'A' == $key ]]; then
-            echo "UP"
+            let is_left_right_key=0
+        elif [[ 'B' == $key ]]; then
+            let is_left_right_key=0
+        elif [[ 'D' == $key ]]; then
+            if [[ $cur_pos -gt 0 ]]; then
+                let cur_pos=cur_pos-1
+            fi
+            let is_left_right_key=1
+        elif [[ 'C' == $key ]]; then
+            if [[ $cur_pos -lt ${#input_cmd} ]]; then
+                let cur_pos=cur_pos+1
+            fi
+            let is_left_right_key=1
+        else
+            let is_arrow_key=0
+            let is_left_right_key=0
         fi
-        if [[ 'B' == $key ]]; then
-            echo "DOWN"
-        fi
-        if [[ 'D' == $key ]]; then
-            echo "LEFT"
-        fi
-        if [[ 'C' == $key ]]; then
-            echo "RIGHT"
-        fi
+    else
+        let is_arrow_key=0
+        let is_left_right_key=0
     fi
     return 0
+}
+
+function clear_line()
+{
+    length=$1
+    empty_line=""
+    for i in $(seq 1 $length); do
+        empty_line="$empty_line "
+    done
+    echo -ne "\r$empty_line"
 }
 
 trap "xexit;" INT QUIT
@@ -65,13 +111,27 @@ esc_flag=0
 c=' '
 while [ 1 ]
 do
+    if [[ $is_left_right_key == 0 ]]; then
+        clear_line ${#input_cmd}
+        prefix_show="$x_prefix_name$input_cmd"
+        echo -ne "\r$prefix_show"
+    fi
+    if [[ $cur_pos != ${#input_cmd} || $is_left_right_key == 1 ]]; then
+        new_prefix_show="$x_prefix_name${input_cmd:0:$cur_pos}"
+        echo -ne "\r$new_prefix_show"
+    fi
     c=`get_key`
  
     if [[ 'q' == $c || '' == $c ]]; then
         xexit
     fi
+    
     handle_arrow_key $c $esc_flag
     let esc_flag=$?
+    if [[ $is_arrow_key == 1 ]]; then
+        continue
+    fi
+
     if [[ '' == $c ]]; then
         handle_enter_key
     fi
@@ -80,5 +140,9 @@ do
     fi
     if [[ '' == $c ]]; then
         handle_backspace_key
+    fi
+    if [[ "$c" =~ ^[a-zA-Z0-9_\ ]$ ]]; then
+        input_cmd="$input_cmd$c"
+        let cur_pos=cur_pos+1
     fi
 done

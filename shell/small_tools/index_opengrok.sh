848be259cca2e9944fa8fa57b3cd9ad71d771183
fenghxu <fenghuai.xu@nokia-sbell.com>
2020-04-29 22:05:58 +0800

    [shell] update index_opengrok.sh

--- shell/small_tools/index_opengrok.sh
+++ shell/small_tools/index_opengrok.sh
@@ -10,9 +10,11 @@ function seconds_to_time()
     second=$((seconds%60))
     minutes=$((seconds/60))
     minute=$((minutes%60))
-    hour=$((minutes/60))
+    hours=$((minutes/60))
+    hour=$((hours%24))
+    days=$((hours/24))
 
-    echo "$hour::$minute::$second"
+    echo "$days days $hour::$minute::$second"
 }
 
 function log()
@@ -23,10 +25,17 @@ function log()
 
 while [ 1 ]
 do
+    cur_day=`date +%d`
     cur_hour=`date +%H`
     echo "cur_hour = $cur_hour"
-    if [[ $cur_hour != 23 ]]; then
+    if [[ $cur_hour != 18 ]]; then
         log "$cur_hour clock, sleep 1 hour"
+        sleep 1h
+        continue
+    fi
+    is_exist=`ps aux | grep opengrok-indexer | grep -v grep`
+    if [[ -n "$is_exist" ]]; then
+        log "opengrok-indexer is running, sleep 1 hour"
         sleep 1h
         continue
     fi
@@ -36,22 +45,48 @@ do
     dirs=`dir $project_dir`
     for d in $dirs
     do
-        if [[ "$d" =~ "19A" ]]; then
-            log "enter $d and rebase"
-            cd $d/gnb
-            pwd
-            git pull --rebase
-            cd -
-        fi
+        log "enter $d and rebase"
+        cd $d/gnb
+        pwd
+        git pull --rebase
+        cd -
     done
+
+    if [[ ! -d $opengrok_dir/etc ]]; then
+        log "etc/ is not exist, create and copy logging.properties from doc/ to etc/"
+        mkdir -p $opengrok_dir/etc
+        cp $opengrok_dir/doc/logging.properties $opengrok_dir/etc/
+    fi
+    if [[ ! -f $opengrok_dir/etc/logging.properties ]]; then
+        log "etc/logging.properties is not exist, copy logging.properties from doc/ to etc/"
+        cp $opengrok_dir/doc/logging.properties $opengrok_dir/etc/
+    fi
+    if [[ ! -d $opengrok_dir/src ]]; then
+        log "src/ is not exist, no codes are needed to index"
+        sleep 1h
+        continue;
+    fi
+    mkdir -p $opengrok_dir/logs
+    mkdir -p $opengrok_dir/data
 
     log "start to run opengrok-indexer"
     start_seconds=$(date +%s)
+
     opengrok-indexer -l debug -J=-Djava.util.logging.config.file=$opengrok_dir/etc/logging.properties -a $opengrok_dir/lib/opengrok.jar -- -s $opengrok_dir/src/ -d $opengrok_dir/data/ -H -P -S -G -W $opengrok_dir/etc/configuration.xml
+
     end_seconds=$(date +%s)
     seconds=$((end_seconds - start_seconds))
     gap=`seconds_to_time $seconds`
     log "opengrok-indexer take $seconds seconds ( $gap )"
 
-    break
+    sudo /usr/local/tomcat/bin/shutdown.sh
+    sudo /usr/local/tomcat/bin/startup.sh
+    sleep 1s
+    sudo /usr/local/tomcat/bin/shutdown.sh
+    sudo /usr/local/tomcat/bin/startup.sh
+
+    now_day=`date +%d`
+    if [[ $cur_day == $now_day ]]; then
+        sleep 1h
+    fi
 done

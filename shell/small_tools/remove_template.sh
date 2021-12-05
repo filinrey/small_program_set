0a1b023d93842c27db4dc69a08f4350465d71794
fenghxu <fenghuai.xu@nokia-sbell.com>
2021-12-05 21:17:26 +0800

    [shell] improve remove_template

--- shell/small_tools/remove_template.sh
+++ shell/small_tools/remove_template.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-#output=`cat $2 | sed -nr "/NRDU-$1/{:a;N;/managedObject>$/{s/.*\ operation(.*)managedObject/\1/p;q};ba}"`
-
 cur_dir=$1
 if [[ ! -d $cur_dir ]]; then
     echo "$cur_dir is not exists"
     return
 fi
 
-file_list=(`grep -rlwE 'CELL|CELLGROUP|NRCELLGROUP|CELLCONFIG' $cur_dir`)
+file_list=(`grep -rlwE 'CELL|CELLGROUP|NRCELLGROUP|CELLCONFIG|ICell|INrCellGroup|ICellConfig|ICellForUeThread|INrCellGroupForUeThread|ICellConfigForUeThread' $cur_dir`)
 echo "totally find ${#file_list[*]} files"
 #echo "${file_list[@]}"
 for file in ${file_list[@]}
@@ -18,40 +16,31 @@ do
     sed -ri 's/_ptr<CELLCONFIG>/_ptr<cell_config::ICellConfigForUeThread>/g' $file
     sed -ri 's/_ptr<NRCELLGROUP>/_ptr<cell_config::INrCellGroupForUeThread>/g' $file
     sed -ri 's/_ptr<CELLGROUP>/_ptr<cell_config::INrCellGroupForUeThread>/g' $file
-    sed -ri '/^\s*template\ [cs].+<.+ICell.*>\;$//g' $file
-    sed -ri '/^\s*template\ [cs].+<.+INrCellGroup.*>\;$//g' $file
-    sed -ri '/^\s*template\ [cs].+<.+ICellConfig.*>\;$//g' $file
-    sed -ri '/^\s*template\ [cs].+<.+ICellForUeThread.*>\;$//g' $file
-    sed -ri '/^\s*template\ [cs].+<.+INrCellGroupForUeThread.*>\;$//g' $file
-    sed -ri '/^\s*template\ [cs].+<.+ICellConfigForUeThread.*>\;$//g' $file
+    sed -ri '/^\s*template\ <.*[typenameCELLCONFIGNRGROUP\ ]{13,}.*>$/{:label; s/(<)typename\ [CELLCONFIGNRGROUP]{4,11}[,]{0,1}[\ ]{0,1}/\1/g; s/,[\ ]{0,1}typename\ [CELLCONFIGNRGROUP]{4,11}//g; s/^\s*template\ <>//g; t label;}' $file
+    sed -ri '/^\s*template\ <.*[classCELLCONFIGNRGROUP\ ]{13,}.*>$/{:label; s/(<)class\ [CELLCONFIGNRGROUP]{4,11}[,]{0,1}[\ ]{0,1}/\1/g; s/,[\ ]{0,1}class\ [CELLCONFIGNRGROUP]{4,11}//g; s/^\s*template\ <>//g; t label;}' $file
+    sed -ri '/<$/{:a;N;s/<\n[CELLCONFIGNRGROUP\ \n,]{4,}>::/::/g;/>::/!ba}' $file
+    sed -ri '/[^p][^t][^r]<$/{:a;N;s/<\n[cprtcellconfigICellConfigNrCellGroupForUeThread_:\ \n,]{14,}>([&]{0,}[\)]{0,}[,]{0,}[\ const=0]{0,}[;]{0,})$/\1/g;/;|\($/!ba}' $file
+    sed -ri 's/^\s*template\ [cs].+<.+ICell.*>\;$//g' $file
+    sed -ri 's/^\s*template\ [cs].+<.+INrCellGroup.*>\;$//g' $file
+    sed -ri 's/^\s*template\ [cs].+<.+ICellConfig.*>\;$//g' $file
+    sed -ri 's/^\s*template\ [cs].+<.+ICellForUeThread.*>\;$//g' $file
+    sed -ri 's/^\s*template\ [cs].+<.+INrCellGroupForUeThread.*>\;$//g' $file
+    sed -ri 's/^\s*template\ [cs].+<.+ICellConfigForUeThread.*>\;$//g' $file
 
-    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template.+ICell.+\)\;$//g;/\)\;$/!ba}' $file
-    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template.+INrCellGroup.+\)\;$//g;/\)\;$/!ba}' $file
-    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template.+ICellConfig.+\)\;$//g;/\)\;$/!ba}' $file
-    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template.+ICellForUeThread.+\)\;$//g;/\)\;$/!ba}' $file
-    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template.+INrCellGroupForUeThread.+\)\;$//g;/\)\;$/!ba}' $file
-    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template.+ICellConfigForUeThread.+\)\;$//g;/\)\;$/!ba}' $file
-    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template.+ICell.*>\;$//g;/>\;$/!ba}' $file
-    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template.+INrCellGroup.*>\;$//g;/>\;$/!ba}' $file
-    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template.+ICellConfig.*>\;$//g;/>\;$/!ba}' $file
-    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template.+ICellForUeThread.*>\;$//g;/>\;$/!ba}' $file
-    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template.+INrCellGroupForUeThread.*>\;$//g;/>\;$/!ba}' $file
-    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template.+ICellConfigForUeThread.*>\;$//g;/>\;$/!ba}' $file
+    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template\ [a-zA-Z].+ICell.+[\)>][const\ ]{0,6}\;$//g;/[\)>][const\ ]{0,6}\;$/!ba}' $file
+    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template\ [a-zA-Z].+INrCellGroup.+[\)>][const\ ]{0,6}\;$//g;/[\)>][const\ ]{0,6}\;$/!ba}' $file
+    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template\ [a-zA-Z].+ICellConfig.+[\)>][const\ ]{0,6}\;$//g;/[\)>][const\ ]{0,6}\;$/!ba}' $file
+    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template\ [a-zA-Z].+ICellForUeThread.+[\)>][const\ ]{0,6}\;$//g;/[\)>][const\ ]{0,6}\;$/!ba}' $file
+    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template\ [a-zA-Z].+INrCellGroupForUeThread.+[\)>][const\ ]{0,6}\;$//g;/[\)>][const\ ]{0,6}\;$/!ba}' $file
+    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template\ [a-zA-Z].+ICellConfigForUeThread.+[\)>][const\ ]{0,6}\;$//g;/[\)>][const\ ]{0,6}\;$/!ba}' $file
     # run 2nd time
-    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template.+ICell.+\)\;$//g;/\)\;$/!ba}' $file
-    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template.+INrCellGroup.+\)\;$//g;/\)\;$/!ba}' $file
-    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template.+ICellConfig.+\)\;$//g;/\)\;$/!ba}' $file
-    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template.+ICellForUeThread.+\)\;$//g;/\)\;$/!ba}' $file
-    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template.+INrCellGroupForUeThread.+\)\;$//g;/\)\;$/!ba}' $file
-    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template.+ICellConfigForUeThread.+\)\;$//g;/\)\;$/!ba}' $file
-    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template.+ICell.*>\;$//g;/>\;$/!ba}' $file
-    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template.+INrCellGroup.*>\;$//g;/>\;$/!ba}' $file
-    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template.+ICellConfig.*>\;$//g;/>\;$/!ba}' $file
-    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template.+ICellForUeThread.*>\;$//g;/>\;$/!ba}' $file
-    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template.+INrCellGroupForUeThread.*>\;$//g;/>\;$/!ba}' $file
-    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template.+ICellConfigForUeThread.*>\;$//g;/>\;$/!ba}' $file
+    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template\ [a-zA-Z].+ICell.+[\)>][const\ ]{0,6}\;$//g;/[\)>][const\ ]{0,6}\;$/!ba}' $file
+    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template\ [a-zA-Z].+INrCellGroup.+[\)>][const\ ]{0,6}\;$//g;/[\)>][const\ ]{0,6}\;$/!ba}' $file
+    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template\ [a-zA-Z].+ICellConfig.+[\)>][const\ ]{0,6}\;$//g;/[\)>][const\ ]{0,6}\;$/!ba}' $file
+    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template\ [a-zA-Z].+ICellForUeThread.+[\)>][const\ ]{0,6}\;$//g;/[\)>][const\ ]{0,6}\;$/!ba}' $file
+    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template\ [a-zA-Z].+INrCellGroupForUeThread.+[\)>][const\ ]{0,6}\;$//g;/[\)>][const\ ]{0,6}\;$/!ba}' $file
+    sed -ri '/^\s*template\ [a-zA-Z]/{:a;N;s/^\s*template\ [a-zA-Z].+ICellConfigForUeThread.+[\)>][const\ ]{0,6}\;$//g;/[\)>][const\ ]{0,6}\;$/!ba}' $file
 
-    sed -ri '/^\s*template\ <.*[typenameCELLCONFIGNRGROUP\ ]{13,}.*>$/{:label; s/(<)typename\ [CELLCONFIGNRGROUP]{4,11}[,]{0,1}[\ ]{0,1}/\1/g;}; s/,[\ ]{0,1}typename\ [CELLCONFIGNRGROUP]{4,11}//g; s/^\s*template\ <>//g; t label;' $file
     sed -ri 's/<[CELLCONFIGNRGROUP,\ ]{4,30}>//g' $file
     sed -ri 's/CELLCONFIG/cell_config::ICellConfigForUeThread/g' $file
     sed -ri 's/NRCELLGROUP/cell_config::INrCellGroupForUeThread/g' $file
@@ -62,3 +51,4 @@ done
 sed -ri 's/class ICell;/class ICellForUeThread;/g' `grep -rlw "class ICell;" $cur_dir`
 sed -ri 's/class ICellConfig;/class ICellConfigForUeThread;/g' `grep -rlw "class ICellConfig;" $cur_dir`
 sed -ri 's/class INrCellGroup;/class INrCellGroupForUeThread;/g' `grep -rlw "class INrCellGroup;" $cur_dir`
+sed -ri 's/([^p][^t][^r])<[cprtcellconfigICellConfigNrCellGroupForUeThread\ ,:_]{14,}>/\1/g' `grep -rlE "<.*cell_config::.+>" $cur_dir`
